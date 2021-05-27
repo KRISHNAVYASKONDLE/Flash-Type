cf3ae40c65e3c3d8401805a56118d5f35fe80cd2
krishnavyas kondle <krishnavyaskondle@gmail.com>
2021-05-27 23:48:08 +0530

    created layout for navigation bar

---
+++ src/components/App/App/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import Nav from "../Nav/Nav";
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        {/* Nav section */}
+        <Nav />
+        <h1>this is vyas</h1>
+        {/* Landing Page */}
+
+        {/* challenge sectin */}
+
+        {/* Footer */}
+      </div>
+    );
+  }
+}
+export default App;
